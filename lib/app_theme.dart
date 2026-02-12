5341017b80ba2804e413b5e1967378574bf51357
NourElDienNwara <noureldiennwara312@gmail.com>
2026-02-12 18:32:50 +0200

    implement app theme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,6 +1,75 @@
 import 'package:flutter/material.dart';
 
+class AppColors {
+  Color black = Color(0xFF171717);
+  Color white = Color(0xFFFFFFFF);
+  Color gray = Color(0xFFA0A0A0);
+}
+
 class AppTheme {
-  static ThemeData lightTheme = ThemeData();
-  static ThemeData darkTheme = ThemeData();
+  static AppColors color = AppColors();
+
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: color.white,
+
+    textTheme: TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w500,
+        color: color.black,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: color.black,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: color.black,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: color.black,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        color: color.black,
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: color.black,
+
+    textTheme: TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w500,
+        color: color.white,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: color.white,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: color.white,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: color.white,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        color: color.white,
+      ),
+    ),
+  );
 }
